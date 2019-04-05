b982270f9c9b1a5081023ba029ee5c1151606b74
Leroy Salih <leroy.salih@gmail.com>
2019-04-05 07:54:33 +0300

    Working on Database-Insert

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, url_for
 from flask_wtf import FlaskForm 
+from flask_debugtoolbar import DebugToolbarExtension
 
 from blueprints.maths import mathsBP
 from blueprints.computing import computingBP
@@ -17,7 +18,9 @@ from models import init_db
 
 
 app = Flask(__name__)
+app.debug = True
 app.config['SECRET_KEY'] = 'thisisasecret'
+toolbar = DebugToolbarExtension(app)
 
 app.register_blueprint(mathsBP, url_prefix='/maths')
 app.register_blueprint(computingBP, url_prefix='/computing')
@@ -31,7 +34,7 @@ init_app(app)
 print (' * {dbUri}'.format(dbUri=app.config['SQLALCHEMY_DATABASE_URI']))
 
 
-@app.route('/')
+@app.route('/') 
 def hello():
   return render_template('index.html', results=None)
 
