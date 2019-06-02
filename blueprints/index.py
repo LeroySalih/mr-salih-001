7085fa6f1f5c5030cbce757fe69e8bd799e97f1b
Leroy Salih <leroy.salih@gmail.com>
2019-06-02 13:41:27 +0300

    Refactoring geometry pages

--- blueprints/index.py
+++ blueprints/index.py
@@ -5,3 +5,8 @@ indexBP = Blueprint('indexBP', __name__, template_folder='templates')
 @indexBP.route('/')
 def index():
   return render_template('index.html', results=None)
+
+
+@indexBP.route('/layout-test')
+def layout_test():
+  return render_template('layout-topic.html')
